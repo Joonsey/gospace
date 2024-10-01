70134ce3fa4c6aee02b1ddd55dcbe4be59da80e9
Johannes <johannes.ronning@outlook.com>
2024-10-01 22:32:01 +0200

    improved drawing, removing eccentricity property

--- main.go
+++ main.go
@@ -22,16 +22,13 @@ type Vec2 struct {
 	Y int
 }
 
-type Entity struct {
-	entity_id        int     // Unique ID for the entity
-	parent           *Entity // Pointer to the parent entity, nil if no parent
+type CelestialBody struct {
+	parent           *CelestialBody // Pointer to the parent entity, nil if no parent
 	orbit            *Orbit  // Orbital parameters, nil if not in an orbit
 	last_update_time int64   // Timestamp of the last position update
 }
 
 type Orbit struct {
-	semi_major_axis   float64 // Semi-major axis of the orbit
-	eccentricity      float64 // Eccentricity of the orbit
 	inclination       float64 // Inclination of the orbit
 	apoapsis          float64 // Apoapsis
 	periapsis         float64 // Periapsis
@@ -53,56 +50,82 @@ func (g *Game) Update() error {
 	}
 
 	if inpututil.IsKeyJustPressed(ebiten.KeyW) {
-		g.orbit.eccentricity += 0.01
+		g.orbit.apoapsis += 10
 	}
+
 	if inpututil.IsKeyJustPressed(ebiten.KeyS) {
-		g.orbit.eccentricity -= 0.01
+		g.orbit.apoapsis -= 10
 	}
+
 	if inpututil.IsKeyJustPressed(ebiten.KeyA) {
-		g.orbit.inclination += 0.01
+		g.orbit.inclination += 1
 	}
 	if inpututil.IsKeyJustPressed(ebiten.KeyD) {
-		g.orbit.inclination -= 0.01
+		g.orbit.inclination -= 1
 	}
 	return nil
 }
 
+// TrueAnomalyToPosition calculates the x, y position on the orbit using the true anomaly.
+func TrueAnomalyToPosition(a, e, inclination, theta float64) (float64, float64) {
+	// Calculate the radial distance for the current angle (true anomaly)
+	r := a * (1 - e*e) / (1 + e*math.Cos(theta))
+
+	// Position in the orbit (without inclination)
+	x := r * math.Cos(theta)
+	y := r * math.Sin(theta)
+
+	return x, y
+}
+
+// DrawOrbit draws the orbit as an ellipse around the center of the screen, with inclination.
 func (g *Game) DrawOrbit(screen *ebiten.Image) {
-	cx, cy := renderWidth/2, renderHeight/2 // Center of the screen
+	cx, cy := screenWidth/2, screenHeight/2 // Center of the screen
 
-	// Semi-minor axis derived from eccentricity: b = a * sqrt(1 - e^2)
-	semi_minor_axis := g.orbit.semi_major_axis * math.Sqrt(1-g.orbit.eccentricity*g.orbit.eccentricity)
+	// Semi-major axis
+	a := (g.orbit.apoapsis + g.orbit.periapsis) / 2
+	// Eccentricity is pre-set
 
-	// Draw the orbit (approximated as an ellipse)
-	numSteps := 100 // Number of segments to draw the ellipse
+	// Draw the orbit by sampling points along the true anomaly (theta)
+	numSteps := 1000
+
+	e := (a - g.orbit.periapsis) / a
+
 	for i := 0; i < numSteps; i++ {
 		t1 := float64(i) / float64(numSteps) * 2 * math.Pi
 		t2 := float64(i+1) / float64(numSteps) * 2 * math.Pi
 
-		x1 := float64(cx) + g.orbit.semi_major_axis*math.Cos(t1)
-		y1 := float64(cy) + semi_minor_axis*math.Sin(t1)
-		x2 := float64(cx) + g.orbit.semi_major_axis*math.Cos(t2)
-		y2 := float64(cy) + semi_minor_axis*math.Sin(t2)
+		// Get positions for the two points on the ellipse
+		x1, y1 := TrueAnomalyToPosition(a, e, g.orbit.inclination, t1)
+		x2, y2 := TrueAnomalyToPosition(a, e, g.orbit.inclination, t2)
 
-		vector.StrokeLine(screen, float32(x1), float32(y1), float32(x2), float32(y2), 1, color.White, false)
+		// Draw the orbit path
+		vector.StrokeLine(screen, float32(float64(cx)+x1), float32(float64(cy)+y1), float32(float64(cx)+x2), float32(float64(cy)+y2), 1, color.White, false)
 	}
 }
 
+// DrawObjectOnOrbit draws the current position of the object on the orbit.
 func (g *Game) DrawObjectOnOrbit(screen *ebiten.Image) {
-	cx, cy := renderWidth/2, renderHeight/2 // Center of the screen
+	cx, cy := screenWidth/2, screenHeight/2 // Center of the screen
 
-	// Semi-minor axis derived from eccentricity: b = a * sqrt(1 - e^2)
-	semi_minor_axis := g.orbit.semi_major_axis * math.Sqrt(1-g.orbit.eccentricity*g.orbit.eccentricity)
+	// Semi-major axis
+	a := (g.orbit.apoapsis + g.orbit.periapsis) / 2
 
-	// Current angle based on the position on the orbit
-	angle := g.orbit.position_on_orbit * 2 * math.Pi
+	// Calculate the current true anomaly based on position in the orbit
+	theta := g.orbit.position_on_orbit * 2 * math.Pi
+	e := (a - g.orbit.periapsis) / a
 
-	// Calculate the current position of the object
-	x := float64(cx) + g.orbit.semi_major_axis*math.Cos(angle)
-	y := float64(cy) + semi_minor_axis*math.Sin(angle)
+	// Get the current position of the object on the orbit
+	x, y := TrueAnomalyToPosition(a, e, g.orbit.inclination, theta)
 
 	// Draw the object as a small circle
-	vector.DrawFilledCircle(screen, float32(x), float32(y), 5, color.RGBA{255, 0, 0, 255}, false)
+	vector.DrawFilledCircle(screen, float32(float64(cx)+x), float32(float64(cy)+y), 5, color.RGBA{255, 0, 0, 255}, false)
+}
+
+func (g *Game) DrawFocalPoint(screen *ebiten.Image) {
+	cx, cy := screenWidth/2, screenHeight/2 // Center of the screen
+
+	vector.DrawFilledCircle(screen, float32(float64(cx)), float32(float64(cy)), 15, color.RGBA{0, 255, 0, 255}, false)
 }
 
 // Draw draws the game screen.
@@ -115,19 +138,19 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	g.DrawOrbit(screen)
 
 	g.DrawObjectOnOrbit(screen)
+
+	g.DrawFocalPoint(screen)
 }
 
 // Layout takes the outside size (e.g., the window size) and returns the (logical) screen size.
 // If you don't have to adjust the screen size with the outside size, just return a fixed size.
-func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return renderWidth, renderHeight
+func (g *Game) Layout(outsideWidth, outsideHeight int) (_, _ int) {
+	return screenWidth, screenHeight
 }
 
 func main() {
 	game := &Game{}
 	game.orbit = Orbit{
-		semi_major_axis:   200,  // Semi-major axis (size of orbit)
-		eccentricity:      0.5,  // Orbit eccentricity (how elliptical the orbit is)
 		inclination:       0,    // Inclination (tilt of the orbit)
 		apoapsis:          300,  // Apoapsis (furthest point)
 		periapsis:         100,  // Periapsis (closest point)
@@ -135,7 +158,8 @@ func main() {
 		position_on_orbit: 0.25, // Starting position on orbit
 	}
 
-	ebiten.SetWindowSize(screenWidth, screenHeight)
+	// Specify the window size as you like. Here, a doubled size is specified.
+	ebiten.SetWindowSize(renderWidth, renderHeight)
 	ebiten.SetWindowTitle("gospace")
 
 	if err := ebiten.RunGame(game); err != nil {
